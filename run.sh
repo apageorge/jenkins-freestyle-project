4c29e4b36e8b3f6fb65fe3dddf39fdce961a93e0
apageorge <147612296+apageorge@users.noreply.github.com>
2023-10-26 14:53:45 +0100

    Update run.sh - second time

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 echo 'Hello from run.sh'
 echo 'This is being run from a jenkins freestyle job'
-echo 'Trying to see if this triggers a new build'
+echo 'Trying to see if this will trigger a new build'
