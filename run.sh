a4a1f97d5b44815a08d352caed3a802b6f459c6a
Aparna <aparnageorge2401@gmail.com>
2023-10-27 15:52:02 +0100

    Test

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
 echo 'Hello from run.sh'
 echo 'This is being run from a jenkins freestyle job'
 echo 'Trying to see if this will trigger a new build'
+echo 'Trying to see if this will trigger a new build -1'
+
